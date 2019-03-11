0bf27398d2512bc840fabef1b4ceb725c1b07ccb
kuidokylm <kuido@koodikaru.ee>
2019-03-11 22:36:08 +0200

    SignatureProfile.LT_TM

--- kampus/src/main/java/web/bdoc/configuration/Seadistus.java
+++ kampus/src/main/java/web/bdoc/configuration/Seadistus.java
@@ -7,7 +7,8 @@ public final class Seadistus {
 	public static SignatureProfile getSignatureProfile()
 	{
 		//return SignatureProfile.B_BES;  //PROD reshiimis ei tee OSCP päringut //kasuta arenduses
-		return SignatureProfile.LT_TM; ///BDOC Time-mark, similar to LT (BDoc 2.1 format).  //kasuta seda produktsioonis    
+		return SignatureProfile.LT_TM; ///BDOC Time-mark, similar to LT (BDoc 2.1 format).  //kasuta seda produktsioonis   
+		//return SignatureProfile.LT; ///BDOC Time-stamp LT - Time-stamp and OCSP confirmation   
 		//Note that BES signatures are missing OCSP (and Timestamp) and therefore are not valid as digital signatures by the law
 	}
 	
