306a775b6c70ac6e8cbbc02833fcbbefc1fd07b4
kuidokylm <kuido@koodikaru.ee>
2020-04-09 19:55:30 +0300

    SignatureProfile.LTA

--- kampus/src/main/java/web/bdoc/configuration/Seadistus.java
+++ kampus/src/main/java/web/bdoc/configuration/Seadistus.java
@@ -1,5 +1,6 @@
 package web.bdoc.configuration;
 
+import org.digidoc4j.DigestAlgorithm;
 import org.digidoc4j.SignatureProfile;
 
 public final class Seadistus {
@@ -8,12 +9,18 @@ public final class Seadistus {
 	{
 		//return SignatureProfile.B_BES;  //PROD reshiimis ei tee OSCP päringut //kasuta arenduses
 		//return SignatureProfile.LT_TM; ///BDOC Time-mark , 
-		return SignatureProfile.LT; ///BDOC Time-stamp LT //kasuta seda produktsioonis     
+		//return SignatureProfile.LT; ///BDOC Time-stamp LT //kasuta seda produktsioonis   
+		return SignatureProfile.LTA; ///BDOC Time-stamp LTA //kasuta seda produktsioonis   
 		//Note that BES signatures are missing OCSP (and Timestamp) and therefore are not valid as digital signatures by the law
 	}
 	
 	public static String getSignatureProfileName()
 	{
 		return getSignatureProfile().name();
+	}
+	
+	public static DigestAlgorithm getDigestAlgorithm()
+	{
+		return org.digidoc4j.DigestAlgorithm.SHA256;
 	}
 }
